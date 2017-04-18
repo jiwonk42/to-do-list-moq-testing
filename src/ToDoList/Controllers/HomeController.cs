5ed569c99dd8730d0f91d8fdecaa5d4ac81d19fc
Cassie Musolf and Jiwon Kang <cassie+jiwon@>
2017-04-18 14:08:23 -0700

    add category name to the index page

--- src/ToDoList/Controllers/HomeController.cs
+++ src/ToDoList/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,20 @@ namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+        private ToDoListContext db = new ToDoListContext();
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(db.Items.ToList());
         }
+
+        //public IActionResult TaskCategoryViewModel()
+        //{
+        //    List<object> myModel = new List<object>();
+        //    myModel.Add(db.Categories.ToList());
+        //    myModel.Add(db.Items.ToList());
+        //    return View(myModel); 
+        //}
     }
 }
---
+++ src/ToDoList/ViewModels/TaskCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.ViewModels
+{
+    public class TaskCategoryViewModel
+    {
+        public IEnumerable<ToDoList.Models.Item> Items { get; set; }
+        public IEnumerable<ToDoList.Models.Category> Categories { get; set; }
+    }
+}
